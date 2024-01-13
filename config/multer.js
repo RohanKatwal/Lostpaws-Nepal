cc15d6799353363ab0ac0220d643b1d1ef50cd0d
rohanwork909 <rohanwork909@gmail.com>
2024-01-13 17:12:12 +0545

    lost found to server

---
+++ config/multer.js
@@ -0,0 +1,44 @@
+const multer  = require('multer')
+const path = require('path')
+
+//store destination
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // console.log(file)
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix +  path.extname(file.originalname))
+    }
+})
+
+//initilization
+const uploadImg = multer({
+    storage: storage,
+    limits: { fileSize: 5*1024*1024},
+    fileFilter: function(req, file, cb){
+        // console.log(file)
+        checkFileType(file, cb);
+      }
+})
+
+const uploaduserImages = uploadImg.fields([{name: 'profile'}])
+const uploadPetImages = uploadImg.fields([{name: 'petImg'}])
+
+//check file type
+function checkFileType(file, cb) {
+    //allowed type
+    const filetypes = /jpeg|jpg|png/
+    //check type
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+    //check mime
+    const mimetype = filetypes.test(file.mimetype)
+    if(mimetype && extname) {
+        return cb(null,true)
+    } else {
+        cb('Error: Image Only!')
+    }
+}
+
+module.exports = {uploaduserImages, uploadPetImages}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,4 +1,5 @@
 const router =require('express').Router();
+const {uploadUserImages, uploadPetImages}= require('../config/multer');
 
 function loggedIn(req,res,next){
     if(req.user){
@@ -16,5 +17,29 @@ router.get('/home',loggedIn, async (req, res) => {
 router.get('/lostfound',loggedIn, async (req, res) => {
     res.render('dashboard/lostfound.ejs',{user:req.user});
 });
+
+router.post('/lostfound',loggedIn,uploadPetImages, async (req, res)=>{
+    console.log("lost page")
+    const {type,pet,petname,gender,colour,breed,age,latitude,longitude} = req.body
+    console.log(req.body.type)
+    console.log(req.body)
+
+    const petImages = req.files.petImg;
+    console.log("0000000000000",petImages)
+
+    // Assuming you want to log file information
+    if (petImages) {
+        petImages.forEach((image) => {
+            console.log("File Information:");
+            console.log("Fieldname:", image.fieldname);
+            console.log("Originalname:", image.originalname);
+            console.log("Encoding:", image.encoding);
+            console.log("Mimetype:", image.mimetype);
+            console.log("Size:", image.size);
+            console.log("Path:", image.path);
+            console.log("-----------------------");
+        });
+    }
+})
 
 module.exports=router;
